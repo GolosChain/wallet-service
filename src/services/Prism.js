85354c040d352f3b30cb88a749d7dfa8f2154859
IvanBlacky <tcherivan@gmail.com>
2019-07-08 18:47:40 +0300

    Refactor: add bulks support Feat: add rewards

--- src/services/Prism.js
+++ src/services/Prism.js
@@ -3,7 +3,6 @@ const BasicService = core.services.Basic;
 const BlockSubscribe = core.services.BlockSubscribe;
 const Logger = core.utils.Logger;
 const MainPrismController = require('../controllers/prism/Main');
-const ServiceMetaModel = require('../models/ServiceMeta');
 
 class Prism extends BasicService {
     constructor() {
@@ -13,20 +12,9 @@ class Prism extends BasicService {
     }
 
     async start() {
-        const {
-            lastBlockSequence,
-            lastBlockTime,
-            lastBlockNum,
-        } = await this._getLastBlockTimeAndSequence();
-
         const subscriber = new BlockSubscribe({
             onlyIrreversible: true,
             blockHandler: this._handleBlock.bind(this),
-        });
-        await subscriber.setLastBlockMetaData({
-            lastBlockSequence,
-            lastBlockTime,
-            lastBlockNum,
         });
 
         await subscriber.start();
@@ -34,42 +22,11 @@ class Prism extends BasicService {
 
     async _handleBlock(block) {
         try {
-            const {
-                sequence: lastBlockSequence,
-                blockTime: lastBlockTime,
-                blockNum: lastBlockNum,
-            } = block;
-
-            await this._setLastBlockTimeAndSequence({
-                lastBlockSequence,
-                lastBlockTime,
-                lastBlockNum,
-            });
             await this._mainPrismController.disperse(block);
         } catch (error) {
             Logger.error('Cant disperse block:', error);
             process.exit(1);
         }
-    }
-
-    async _getLastBlockTimeAndSequence() {
-        return await ServiceMetaModel.findOne(
-            {},
-            {
-                _id: false,
-                lastBlockSequenceSequence: true,
-                lastBlockTime: true,
-                lastBlockNum: true,
-            },
-            { lean: true }
-        );
-    }
-
-    async _setLastBlockTimeAndSequence({ lastBlockSequence, lastBlockTime, lastBlockNum }) {
-        await ServiceMetaModel.updateOne(
-            {},
-            { $set: { lastBlockSequence, lastBlockNum, lastBlockTime } }
-        );
     }
 }
 
--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,6 +1,10 @@
 const core = require('gls-core-service');
 const Logger = core.utils.Logger;
 const BigNum = core.types.BigNum;
+const VestingBalance = require('../models/VestingBalance');
+const VestingStat = require('../models/VestingStat');
+const BalanceModel = require('../models/Balance');
+const TokenModel = require('../models/Token');
 
 class Utils {
     static async extractArgumentList({ args, fields }) {
@@ -106,6 +110,150 @@ class Utils {
         return (
             new BigNum(quantity.amount).shiftedBy(-quantity.decs).toString() + ' ' + quantity.sym
         );
+    }
+
+    static async convertTokensToVesting({ tokens }) {
+        // todo: migrate this to Utils
+
+        const { decs, amount } = await Utils.checkAsset(tokens);
+
+        await Utils.checkDecsValue({ decs, requiredValue: 3 });
+
+        const { balance, supply } = await Utils.getVestingSupplyAndBalance();
+
+        return Utils.convertAssetToString({
+            sym: 'GOLOS',
+            amount: Math.round((amount * supply) / balance),
+            decs: 6,
+        });
+    }
+
+    static async getVestingInfo() {
+        const vestingStat = await VestingStat.findOne();
+
+        if (!vestingStat) {
+            return {};
+        }
+
+        return { stat: vestingStat.stat };
+    }
+
+    static async getBalance({ userId, currencies, type }) {
+        let res = {
+            userId,
+        };
+
+        let tokensMap = {};
+
+        if (type !== 'liquid') {
+            const {
+                vesting: total,
+                delegated: outDelegate,
+                received: inDelegated,
+            } = await Utils.getVestingBalance({ account: userId });
+
+            res.vesting = { total, outDelegate, inDelegated };
+        }
+
+        if (type !== 'vesting') {
+            const balanceObject = await BalanceModel.findOne({ name: userId });
+
+            if (balanceObject) {
+                res.liquid = {};
+                if (currencies.includes('all')) {
+                    const allCurrencies = await TokenModel.find({});
+                    for (const currency of allCurrencies) {
+                        tokensMap[currency.sym] = true;
+                    }
+                } else {
+                    for (const token of currencies) {
+                        tokensMap[token] = true;
+                    }
+                }
+                for (const tokenBalance of balanceObject.balances) {
+                    const { sym, quantityRaw } = await Utils.parseAsset(tokenBalance);
+                    if (tokensMap[sym]) {
+                        res.liquid[sym] = quantityRaw;
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+
+    static async getVestingSupplyAndBalance() {
+        const vestingStat = await Utils.getVestingInfo();
+        const vestingBalance = await Utils.getBalance({
+            userId: 'gls.vesting',
+            currencies: ['GOLOS'],
+            type: 'liquid',
+        });
+
+        await Utils.checkVestingStatAndBalance({
+            vestingBalance,
+            vestingStat: vestingStat.stat,
+        });
+
+        const balance = await Utils.checkAsset(vestingBalance.liquid.GOLOS);
+        const supply = await Utils.checkAsset(vestingStat.stat);
+
+        return {
+            balance: balance.amount,
+            supply: supply.amount,
+        };
+    }
+
+    static async getVestingBalance({ account }) {
+        const vestingBalance = await VestingBalance.findOne({ account });
+
+        if (!vestingBalance) {
+            return {};
+        }
+
+        vestingBalance.vesting = Utils.parseAsset(vestingBalance.vesting).quantityRaw;
+        vestingBalance.delegated = Utils.parseAsset(vestingBalance.delegated).quantityRaw;
+        vestingBalance.received = Utils.parseAsset(vestingBalance.received).quantityRaw;
+
+        const { quantityRaw: vestingInGolos } = await Utils.convertVestingToToken({
+            vesting: vestingBalance.vesting,
+            type: 'parsed',
+        });
+        const { quantityRaw: delegatedInGolos } = await Utils.convertVestingToToken({
+            vesting: vestingBalance.delegated,
+            type: 'parsed',
+        });
+        const { quantityRaw: receivedInGolos } = await Utils.convertVestingToToken({
+            vesting: vestingBalance.received,
+            type: 'parsed',
+        });
+
+        return {
+            account,
+            vesting: { GESTS: vestingBalance.vesting, GOLOS: vestingInGolos },
+            delegated: { GESTS: vestingBalance.delegated, GOLOS: delegatedInGolos },
+            received: { GESTS: vestingBalance.received, GOLOS: receivedInGolos },
+        };
+    }
+
+    static async convertVestingToToken({ vesting, type }) {
+        // todo: migrate this to Utils
+
+        const { decs, amount } = await Utils.checkAsset(vesting);
+
+        await Utils.checkDecsValue({ decs, requiredValue: 6 });
+
+        const { balance, supply } = await Utils.getVestingSupplyAndBalance();
+        const resultString = Utils.convertAssetToString({
+            sym: 'GOLOS',
+            amount: Math.round((amount * balance) / supply),
+            decs: 3,
+        });
+
+        if (type === 'string') {
+            return resultString;
+        }
+        return Utils.parseAsset(resultString);
     }
 }
 
