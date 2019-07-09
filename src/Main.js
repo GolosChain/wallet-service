4c1da47887bce43c95e2c13d4e0a8840c0918181
Ilya Lebedev <bacherko@gmail.com>
2019-07-09 13:03:44 +0300

    fix: add ServiceMeta default creation

--- src/Main.js
+++ src/Main.js
@@ -3,6 +3,7 @@ const BasicMain = core.services.BasicMain;
 const env = require('./data/env');
 const Prism = require('./services/Prism');
 const Connector = require('./services/Connector');
+const ServiceMetaModel = require('./models/ServiceMeta');
 
 class Main extends BasicMain {
     constructor() {
@@ -13,6 +14,14 @@ class Main extends BasicMain {
 
         this.startMongoBeforeBoot();
         this.addNested(prism, connector);
+    }
+
+    async boot() {
+        if ((await ServiceMetaModel.countDocuments()) === 0) {
+            const model = new ServiceMetaModel();
+
+            await model.save();
+        }
     }
 }
 
